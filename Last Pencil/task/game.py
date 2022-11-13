2577ff2b1abd293ef4f9c91ea23cc3333fc3c31c
Acestus <architect@acestus.com>
2022-11-13 09:17:46 -0600

    wrote stage 2

--- Last Pencil/task/game.py
+++ Last Pencil/task/game.py
@@ -1,2 +1,10 @@
-print("|||")
-print("Your turn!")
+number_of_pencils = int(input("How many pencils would you like to use:\n"))
+name1 = "Idaho"
+name2 = "Leto"
+pencil_inventory = ""
+first_player= input(f"Who will be first? ({name1}, {name2}):\n")
+
+for i in range(number_of_pencils):
+    pencil_inventory += "|"
+print(f"\n{pencil_inventory}")
+print(f"{first_player} is going first!")
